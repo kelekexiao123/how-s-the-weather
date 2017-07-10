bc46db16c695fe5000d1698543eb7ba9bfd9e69f
kelekexiao123 <250407778@qq.com>
2017-07-10 20:56:33 +0800

    complete the base function on web

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,143 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <el-button @click.native="startHacking">Let's do it</el-button>
-  </div>
+    <div id="app" v-loading="searchLoading" element-loading-text="正在夜观星象...">
+        <div class="today" id="today">
+            <el-row>
+                <el-col :span="24">
+                    <h1 class="city">{{ city }}</h1>
+                </el-col>
+            </el-row>
+            <el-row type="flex" justify="center" align="middle">
+                <el-col :span="6">
+                    <img :src="now.nowPic" class="now-img">
+                </el-col>
+                <el-col :span="6">
+                    <h1>{{ now.nowTxt }}</h1>
+                </el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="24">
+                    <h1>{{ now.nowTmp }}℃</h1>
+                </el-col>
+            </el-row>
+            <el-row type="flex" justify="center">
+                <el-col :span="6">
+                    <h4>{{ now.nowDir }}</h4>
+                </el-col>
+                <el-col :span="6">
+                    <h4>{{ now.nowSc }}</h4>
+                </el-col>
+                <el-col :span="8">
+                    <h4>相对湿度{{ now.nowHum }}%</h4>
+                </el-col>
+            </el-row>
+        </div>
+    
+        <el-row :gutter="10">
+            <el-col :span="8" v-for="(item, index) in forecast" :key="'item' + index">
+                <div class="forecastItem">
+                    <h4>{{ item.forecastDate }}</h4>
+                    <img :src="item.forecastPic">
+                    <h1>{{ item.forecastTmp }}</h1>
+                    <h4>{{ item.forecastTxt }}</h4>
+                </div>
+            </el-col>
+        </el-row>
+    
+    </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Use Vue 2.0 Today!'
-    }
-  },
+// import ForecastWeather from './components/ForecastWeather'
 
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It Works',
-        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
-        duration: 6000
-      })
+export default {
+    name: 'app',
+    data() {
+        return {
+            searchLoading: true,
+            city: '长沙',
+            now: {
+                nowPic: '',
+                nowTxt: '',
+                nowTmp: '',
+                nowDir: '',
+                nowSc: '',
+                nowHum: '',
+            },
+
+            forecast: []
+        }
+    },
+    created() {
+        fetch(`https://free-api.heweather.com/v5/weather?city=${this.city}&key=df3cc12e6f074b24bce8337997cc7641`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                let nowData = data.HeWeather5[0].now
+                this.now.nowPic = `http://files.heweather.com/cond_icon/${nowData.cond.code}.png`
+                this.now.nowTxt = nowData.cond.txt
+                this.now.nowTmp = nowData.tmp
+                this.now.nowDir = nowData.wind.dir
+                this.now.nowSc = nowData.wind.sc
+                this.now.nowHum = nowData.hum
+
+                let forecastDatas = data.HeWeather5[0].daily_forecast
+                forecastDatas.forEach((data, index) => {
+                    console.log(data)
+                    let tempData = {}
+                    tempData.forecastDate = data.date
+                    tempData.forecastPic = `http://files.heweather.com/cond_icon/${data.cond.code_d}.png`
+                    tempData.forecastTmp = data.cond.txt_d
+                    tempData.forecastTxt = `${data.tmp.min} ~ ${data.tmp.max}℃`
+                    this.forecast.push(tempData)
+                })
+
+                this.searchLoading = false
+            })
+            .catch(err => {
+                console.log(err)
+                this.searchLoading = false
+            })
     }
-  }
+
 }
 </script>
 
 <style>
-body {
-  font-family: Helvetica, sans-serif;
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*text-shadow: 1px 1px 6px #333;*/
+    margin-top: 60px;
+}
+
+.today {
+    padding-bottom: 30px;
+}
+
+
+.now-img {
+    width: 60px;
+}
+
+.forecastItem {
+    border: 1px solid #ccc;
+    padding: 10px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, .3);
+}
+
+.forecastItem img {
+    width: 100px;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    margin: 5px;
 }
 </style>
---
+++ src/components/NowWeather.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="todayWeather">
+        <h1 class="city">厦门</h1>
+        <div class="row_detail">
+            <img src="http://files.heweather.com/cond_icon/104.png">
+            <h1>19
+                <span>℃</span>
+            </h1>
+        </div>
+        <div class="wind">
+            <h2>阴</h2>
+            <h4>风速 20 湿度 89%</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todayWeather',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
